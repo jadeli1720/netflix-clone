ed72449b5e4e8824f43bca2027c4e558302302ab
jadeli1720 <jadeli1720@gmail.com>
2022-05-20 20:33:33 -0600

    initial commit

---
+++ src/components/homeScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function HomeScreen() {
+    return (
+        <div className='homeScreen'>
+            {/* Nav */}
+            {/* Banner */}
+            {/* Movie Rows */}
+        </div>
+    )
+}
+
+export default HomeScreen
