4c61bbd6c2f44696d122bcb467d97a9d63ae993d
jadeli1720 <jadeli1720@gmail.com>
2023-02-04 22:47:30 -0700

    style file clean up and organization

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import "./styles/App.scss";
 import * as ROUTES from './constants/routes'
 import SignIn from "./components/signInPage";
 import SignUp from "./components/signUpPage";
