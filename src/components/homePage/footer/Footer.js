4a9b8562675656a4d48a8508ac50d8182a4be77a
jadeli1720 <jadeli1720@gmail.com>
2023-02-09 20:42:13 -0700

    Footer done

--- src/components/homePage/footer/Footer.js
+++ src/components/homePage/footer/Footer.js
@@ -1,46 +1,45 @@
 import React from 'react';
+import * as ROUTES from "../../../constants/routes";
 import './footer.scss';
-
-//TODO: Could we achieve the breaks with padding and margins?
 
 export default function Footer() {
   return (
-    <div  className="footerContainer">
-      <div className='footerTitle'>Questions? Contact Us.</div>
-      <div className="break" ></div>
-      <div className="row" >
-        <div className='column'>
-          <a href="#">FAQ</a>
-          <a href="#">Investor Relations</a>
-          <a href="#">Ways to Watch</a>
-          <a href="#">Corporate Information</a>
-          <a href="#">Netflix Originals</a>
-        </div>
+    <>
+      <div  className="footerContainer">
+        <div className='footerTitle'>Questions? Contact Us.</div>
+        
+        <div className="row" >
+          <div className='column'>
+            <a href={ROUTES.HOME}>FAQ</a>
+            <a href={ROUTES.HOME}>Investor Relations</a>
+            <a href={ROUTES.HOME}>Ways to Watch</a>
+            <a href={ROUTES.HOME}>Corporate Information</a>
+            <a href={ROUTES.HOME}>Netflix Originals</a>
+          </div>
 
-        <div className='column'>
-          <a href="#">Help Center</a>
-          <a href="#">Jobs</a>
-          <a href="#">Terms of Use</a>
-          <a href="#">Contact Us</a>
-        </div>
+          <div className='column'>
+            <a href={ROUTES.HOME}>Help Center</a>
+            <a href={ROUTES.HOME}>Jobs</a>
+            <a href={ROUTES.HOME}>Terms of Use</a>
+            <a href={ROUTES.HOME}>Contact Us</a>
+          </div>
 
-        <div className='column'>
-          <a href="#">Account</a>
-          <a href="#">Redeem gift cards</a>
-          <a href="#">Privacy</a>
-          <a href="#">Speed Test</a>
-        </div>
+          <div className='column'>
+            <a href={ROUTES.HOME}>Account</a>
+            <a href={ROUTES.HOME}>Redeem gift cards</a>
+            <a href={ROUTES.HOME}>Privacy</a>
+            <a href={ROUTES.HOME}>Speed Test</a>
+          </div>
 
-        <div className='column'>
-          <a href="#">Media Center</a>
-          <a href="#">Buy gift cards</a>
-          <a href="#">Cookie Preferences</a>
-          <a href="#">Legal Notices</a>
+          <div className='column'>
+            <a href={ROUTES.HOME}>Media Center</a>
+            <a href={ROUTES.HOME}>Buy gift cards</a>
+            <a href={ROUTES.HOME}>Cookie Preferences</a>
+            <a href={ROUTES.HOME}>Legal Notices</a>
+          </div>
         </div>
       </div>
-      <div className="break" ></div>
-      <div className="copyright"></div>
-    </div>
+    </>
   )
 }
 
